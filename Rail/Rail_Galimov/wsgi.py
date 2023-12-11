479c2af06b0d0b29a5fccbdb9a46854c39e79cb9
shelzyxpython <144206667+shelzyxpython@users.noreply.github.com>
2023-12-11 19:25:54 +0200

    Add files via upload

---
+++ Rail/Rail_Galimov/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI Rail_Galimov for Rail_Galimov project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rail_Galimov.settings')
+
+application = get_wsgi_application()
